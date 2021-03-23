5fadf3bc0b9a38c0e102ce642e48488b52bdc3f8
wube <wubshet.kifle@newwave.com>
2021-03-23 17:35:52 -0400

    Added release number

--- src/shared/layout/footer/footer.tsx
+++ src/shared/layout/footer/footer.tsx
@@ -7,12 +7,15 @@ export const Footer = () => {
       <div className="usa-footer__secondary-section footer">
         <div className="grid-container">
           <div className="grid-row grid-gap">
-            <div className="usa-footer__logo grid-row mobile-lg:grid-col-6 mobile-lg:grid-gap-2">
+            <div className="usa-footer__logo grid-row mobile-lg:grid-col-6 mobile-lg:grid-gap-1">
               <div className="mobile-lg:grid-col-auto">
                 <p className="usa-text-small">Powered by</p>
               </div>
               <div className="mobile-lg:grid-col-auto">
                 <img src={faasLogo} alt="footer logo" style={{ marginTop: '-20px' }} width="150" height="20" />
+              </div>
+              <div className="mobile-lg:grid-col-auto">
+              <p className="usa-text-small"> | Release 1.0</p>
               </div>
             </div>
           </div>
